a872a60b1b6176eec4ffbe8a8a3c1951b296fd1b
EnosPebrian <enospebrian1@gmail.com>
2023-09-12 11:11:56 +0700

    update validator

--- src/middlewares/validators/user.validator.js
+++ src/middlewares/validators/user.validator.js
@@ -2,3 +2,11 @@ const newCashierAccount = (req, res, next) => {
   //bersihin account
   // const {username}
 };
+
+const adminValidator = (req, res, next) => {
+  //checkrole
+};
+const cashierValidator = (req, res, next) => {
+  //check enable disable status
+  //check role
+};
