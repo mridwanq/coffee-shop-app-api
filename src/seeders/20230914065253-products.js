46a576d37ad4efa0d1fc625c0f3f69501d744fa9
EnosPebrian <enospebrian1@gmail.com>
2023-09-15 13:18:56 +0700

    update seeders

---
+++ src/seeders/20230914065253-products.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const { sequelize } = require("../models");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert("Products", [
+      {
+        id: 1,
+        productName: "Kopi Tubruk",
+        imageName: "eventImage_2023-09-14-15-20-49.jpeg",
+        desc: "Cap Jagung",
+        price: 16000,
+        stock: 999,
+        category: 1,
+        createdAt: sequelize.fn("NOW"),
+        updatedAt: sequelize.fn("NOW"),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
---
+++ src/seeders/20230914065253-transaction.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const { sequelize } = require("../models");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert("Transactions", [
+      {
+        id: 1,
+        order_type: 1,
+        isPaid: 0,
+        staff: 1,
+        createdAt: sequelize.fn("NOW"),
+        updatedAt: sequelize.fn("NOW"),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
