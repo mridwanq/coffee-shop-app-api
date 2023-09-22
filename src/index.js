80af78b664434efe41cff9436811d92f5a93205a
EnosPebrian <enospebrian1@gmail.com>
2023-09-22 14:54:15 +0700

    update index js cors

--- src/index.js
+++ src/index.js
@@ -5,27 +5,18 @@ const bearertoken = require("express-bearer-token");
 const routers = require("./routes/index");
 const db = require("./models");
 const app = express();
-
-//socket-io
-const http = require("http");
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server, { cors: { origin: "*" } });
-global.io = io;
-module.exports = { io };
-
-const PORT = process.env.PORT || 2000;
-
 app.use(cors());
 app.use(express.json());
 app.use(bearertoken());
 
-app.use('/users', routers.userRouter);
-app.use('/products', routers.productRouter);
-app.use('/category', routers.categoryRouter);
-app.use('/transactions', routers.transactionRouter);
-app.use('/transaction_details', routers.transactionDetailRouter);
-app.use('/report', routers.reportRouter);
+const PORT = process.env.PORT || 2000;
+
+app.use("/users", routers.userRouter);
+app.use("/products", routers.productRouter);
+app.use("/category", routers.categoryRouter);
+app.use("/transactions", routers.transactionRouter);
+app.use("/transaction_details", routers.transactionDetailRouter);
+app.use("/report", routers.reportRouter);
 
 app.use(
   "/public/product",
@@ -36,6 +27,13 @@ app.get("/test", (req, res) => {
   res.send("welcome my coffee shop API");
 });
 
+//socket-io
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, { cors: { origin: "*" } });
+global.io = io;
+module.exports = { io };
 server.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
   // db.sequelize.sync({ alter: true });
